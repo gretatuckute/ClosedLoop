4106ba68326ca2661767024ca908032d25823320
Sofie Therese Hansen <sofha@dtu.dk>
2019-04-01 13:07:11 +0200

    Update random_1subject.py

--- Scripts/random_1subject.py
+++ Scripts/random_1subject.py
@@ -3,6 +3,8 @@
 Created on Wed Mar  6 14:37:48 2019
 
 @author: sofha
+Randomly assign subject as feedback subjects or as matched controls
+
 """
 
 import csv
@@ -15,10 +17,14 @@ from random import shuffle
 import os
 import numpy as np
 import shutil
+from paths import script_path_init, subject_path_init
+
+subject_path = subject_path_init()
 
 
 
 def copy_files(from_file,to_file):
+    # Copy images indices files to subject folder and rename 
     shutil.copy(from_file+'_day_1.csv',to_file+'_day_1.csv')
     shutil.copy(from_file+'_day_2.csv',to_file+'_day_2.csv')
     shutil.copy(from_file+'_day_3.csv',to_file+'_day_3.csv')
@@ -27,9 +33,18 @@ def copy_files(from_file,to_file):
 
 def copy_alpha(from_file,to_file):
     shutil.copy(from_file,to_file)
+    
 def read_re_move_indices(subjects_dir,sub,feedback_from=None):
+    ''' Copy image indices files into suject folfer
+    Inputs:
+        subjects_dir: path to main subject directory
+        sub: subject ID
+        feedback_from: None if feedback subject, else subject ID of matched feedback subject
+    '''
+    
     indices_dir=subjects_dir+'createIndices\\'
     if feedback_from==None: # if feedback person
+        # Look for available/unused createIndices files
         avail_indices_fname=indices_dir+'avail_indices.txt'
         f_indices=open(avail_indices_fname,'r')
         avail_indices=f_indices.readlines()
@@ -44,16 +59,21 @@ def read_re_move_indices(subjects_dir,sub,feedback_from=None):
         f_indices.close()
     else: # if control get indices from feedback_from
         indices_fname=subjects_dir+feedback_from+'\\createIndices_'+feedback_from
-        #alphafile_src = subjects_dir + feedback_from + '\\alpha_subjID_' + feedback_from + '.csv'
-        #alphafile_dest = subjects_dir  + sub + '\\alpha_subjID_' + sub + '.csv'
-        #copy_alpha(alphafile_src,alphafile_dest) 
+
     
     indices_to_fname=subjects_dir+sub+'\\'+'createIndices_'+sub#+'_day_1'.csv
     copy_files(indices_fname,indices_to_fname)   
-#    avail_indices2=[avail for avail in avail_indices if indices not in avail]
-    
+ 
 
 def feedback_txt(subjects_dir,sub,feedback,feedback_from=None):
+    ''' Generates txt file containing whether subject is feedback (1) or control(0). 
+    If control subject, the matched feedback subject is written in line 2
+    Inputs:
+        subjects_dir: path to main subject directory
+        sub: subject ID
+        feedback: whether feedback (1) or control (0) subject
+        feedback_from: if control subject which feedback subject to match with
+    '''
     if feedback_from==None:
         feedback_from=sub
     fname=subjects_dir+sub+'\\feedback_subjID'+sub+'.txt'
@@ -64,6 +84,7 @@ def feedback_txt(subjects_dir,sub,feedback,feedback_from=None):
 
     
 def assign_remove_avail_feedback_sub(group_fname):
+    # Get a matched feedback subject for a newly assigned control subject from the relevant group
     f=open(group_fname,'r')
     available_subjectIDs=f.readlines()
     f.close()
@@ -77,6 +98,8 @@ def assign_remove_avail_feedback_sub(group_fname):
     return feedback_from
     
 def add_avail_feedback_sub(add_subjectID,group_fname):
+    # After the neurofeedback run of a feedback subject add subject ID to list of 
+    #available feedback subjects
     file_exist=os.path.isfile(group_fname)
     if file_exist:
         f_group=open(group_fname,'a')
@@ -88,17 +111,21 @@ def add_avail_feedback_sub(add_subjectID,group_fname):
         f_group.close()
     
 def assign_feedback(subjects_dir,sub,group_fname):
+    # if subject is assigned to be feedback subject
     feedback_txt(subjects_dir,sub,1)
     read_re_move_indices(subjects_dir,sub)
     #add_avail_feedback_sub(sub,group_fname)
     
 def assign_control(subjects_dir,sub,group_fname):
+    # if subject is assigned to be control subject
     feedback_from=assign_remove_avail_feedback_sub(group_fname)
     feedback_txt(subjects_dir,sub,0,feedback_from)
     read_re_move_indices(subjects_dir,sub,feedback_from)
 
 def gen_group_fname(sub):
-    subjects_dir = 'C:\\Users\\nicped\\Documents\\GitLab\\project\\SUBJECTS\\'#''C:\\Users\\sofha\\Documents\\GitLab\\project\\SUBJECTS\\'
+    # Find group of subject sub (currently not used)
+    
+    subjects_dir = subject_path+'\\'
     data_file = 'C:\\Users\\nicped\\Documents\\google_drive\\booking.xlsx'#''C:\\Users\\sofha\\Documents\\GitLab\\project\\documents_experiment\\bookings.xlsx'
     colnames = ['name', 'subID', 'age', 'gender', 'hand', 'day1_date', 'day1_time', 'day2_date', 'day2_time',
                 'day3_date', 'day3_time']
@@ -121,15 +148,15 @@ def gen_group_fname(sub):
     subjID[ID]
     filename = 'ageRange' + str(age_range) + '_gender' + gender[ID] + '_hand' + hand[ID] + '.txt'
 
-    data_dir = subjects_dir + '\\randomization_files\\'
+    data_dir = subjects_dir + 'randomization_files\\'
     group_fname = data_dir + filename
     return group_fname
 
-
-
 def assign_subject(sub):
-    subjects_dir = 'C:\\Users\\nicped\\Documents\\GitLab\\project\\SUBJECTS\\'#''C:\\Users\\sofha\\Documents\\GitLab\\project\\SUBJECTS\\'
-    data_file = 'C:\\Users\\nicped\\Documents\\google_drive\\booking.xlsx'#''C:\\Users\\sofha\\Documents\\GitLab\\project\\documents_experiment\\bookings.xlsx'
+    # assign subject sub to be either control or feedback
+    
+    subjects_dir = subject_path+'\\'
+    data_file = 'C:\\Users\\nicped\\Documents\\google_drive\\booking.xlsx'# file containing information of the subject
     colnames = ['name', 'subID', 'age', 'gender', 'hand', 'day1_date', 'day1_time', 'day2_date', 'day2_time',
                 'day3_date', 'day3_time']
     data = pd.read_excel(data_file, names=colnames, converters={'subID': str})
@@ -151,10 +178,9 @@ def assign_subject(sub):
     subjID[ID]
     filename = 'ageRange' + str(age_range) + '_gender' + gender[ID] + '_hand' + hand[ID] + '.txt'
 
-    data_dir = subjects_dir + '\\randomization_files\\'
+    data_dir = subjects_dir + 'randomization_files\\'
     group_fname = data_dir + filename
     file_exist = os.path.isfile(group_fname)
-    indices_dir = subjects_dir + 'createIndices\\'
 
     if file_exist:
         f = open(group_fname, 'r')
@@ -163,7 +189,7 @@ def assign_subject(sub):
         n_avail = len(available_subjectIDs)
         if n_avail:
             #print('Available feedback matches, entering randomization')
-            if n_avail > 3:  # CONTROL
+            if n_avail > 2:  # CONTROL
                 #print('More than two avail, assigning control')
                 assign_control(subjects_dir, sub, group_fname)
             else:
